e2303c9d0931bc013f5b7c2ca79ab32922b1f24f
Jakob Westhoff <jakob@westhoffswelt.de>
2015-05-11 22:53:25 +0200

    feat(Container): Implement container

--- Library/Container.js
+++ Library/Container.js
@@ -1,5 +1,13 @@
-export class Container {
-    constructor(solutions) {
-        this.solutions = solutions;
+export default class Container {
+    constructor(factories) {
+        this.factories = factories;
+    }
+
+    get(name) {
+        if (this.factories.get(name) === undefined) {
+            throw new Error(`Required dependency ${name} could not be found.`);
+        }
+
+        return this.factories.get(name)(this);
     }
 }
